89b6277bd08084f6563b2d3125ab2f165b864794
joseffsmith <joe@rapidtender.com>
2019-11-28 09:04:14 +0000

    moving to static html

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Cardiff student houses
 
-Website using `create-react-app` to advertise student houses in Cardiff.
---
+++ index.js
@@ -0,0 +1,24 @@
+var mymap = L.map('mapid').setView([51.491813, -3.169616], 14);
+L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+    maxZoom: 18,
+}).addTo(mymap);
+var glenroy = L.circle([51.491813, -3.169616], {
+    color: '#a12a2a',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 30
+}).addTo(mymap);
+var mackintosh = L.circle([51.493712, -3.172461], {
+    color: '#a12a2a',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 30
+}).addTo(mymap);
+
+glenroy.on('click', e => {
+  window.open(`https://www.google.com/maps/search/?api=1&query=51.491813,-3.169616&query_place_id=EhxHbGVucm95IFN0LCBDYXJkaWZmIENGMjQsIFVLIi4qLAoUChIJ0SWjNsAcbkgR3RVu5ESw8BYSFAoSCamRx0IRO1oCEXoliDJDoPjE`,"_blank")
+})
+mackintosh.on('click', e => {
+  window.open(`https://www.google.com/maps/search/?api=1&query=51.493712,-3.172461&query_place_id=Eh9NYWNLaW50b3NoIFBsLCBDYXJkaWZmIENGMjQsIFVLIi4qLAoUChIJ-yrJHZUcbkgRRRw6sv8W-KsSFAoSCamRx0IRO1oCEXoliDJDoPjE`,"_blank")
+})
